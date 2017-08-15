a00ef651cd761728f25a0ebfeebe2aeffa1ad096
Justin Helmer <justin.helmer@macys.com>
2017-08-15 08:54:23 -0700

    fix copy logic

--- app/index.js
+++ app/index.js
@@ -42,13 +42,19 @@ module.exports = class extends Generator {
 
   write() {
     this.fs.copyTpl(
-      this.templatePath('**/*.@(html|js|md|vue|css|scss)'),
+      this.templatePath('{.*,*/**}'), // matches all files, including root dotfiles
       this.destinationPath(),
-      { config: this.config, features: this.features }
+      { config: this.config, features: this.features },
+      null,
+      {
+        globOptions: {
+          // public directory contains binary assets (i.e. images) that will throw EJS error if passed to copyTpl()
+          ignore: this.templatePath('public/**')
+        }
+      }
     );
 
-    this.fs.copy(this.templatePath('**/.*'), this.destinationPath()); // dotfiles
-    this.fs.copy(this.templatePath('**/*.@(png|jpg|gif)'), this.destinationPath());
+    this.fs.copy(this.templatePath('public/**'), this.destinationPath());
 
     if (!this.features.foundation) {
       this.fs.delete(this.destinationPath('src/lib/foundation.js'));
